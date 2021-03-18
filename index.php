466c285709861c182df14fe162035b22f57dc9db
Marisol Cabañas <72290200+mariscf99@users.noreply.github.com>
2021-03-18 11:55:28 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,276 @@
+
+<html lang="en">
+  <head>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v4.1.1">
+    <title>Pastoral Vocacional</title>
+    <!-- Bootstrap CSS -->
+    
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/fontawesome-all.min.css">
+    <link rel="stylesheet" href="css/estilo.css">
+
+    <!-- Favicons -->
+<link rel="apple-touch-icon" href="/docs/4.5/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+<link rel="icon" href="/docs/4.5/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+<link rel="icon" href="/docs/4.5/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+<link rel="manifest" href="/docs/4.5/assets/img/favicons/manifest.json">
+<link rel="mask-icon" href="/docs/4.5/assets/img/favicons/safari-pinned-tab.svg" color="#563d7c">
+<link rel="icon" href="/docs/4.5/assets/img/favicons/favicon.ico">
+<meta name="msapplication-config" content="/docs/4.5/assets/img/favicons/browserconfig.xml">
+<meta name="theme-color" content="#563d7c">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+        <!-- Custom styles for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700,900" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/blog.css" rel="stylesheet">
+  </head>
+  <body>
+
+    <div class="container">
+  <header class="blog-header py-3">
+    <div class="row flex-nowrap justify-content-between align-items-center">
+      <div class="col-4 pt-1">
+        <a class="text-muted" data-toggle="modal" data-target="#registrar-modal" href="#">Registrarse</a>
+      </div>
+      <div class="col-4 text-center">
+        <a class="blog-header-logo text-dark" href="#">Unilever</a>
+      </div>
+
+
+      <div class="col-4 d-flex justify-content-end align-items-center">
+        <a class="text-muted" href="#" aria-label="Search">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="mx-3" role="img" viewBox="0 0 24 24" focusable="false"><title>Search</title><circle cx="10.5" cy="10.5" r="7.5"/><path d="M21 21l-5.2-5.2"/></svg>
+        </a>
+        <h4 class="btn btn-sm btn-outline-secondary"id="Login"><a data-toggle="modal" data-target="#login-modal">Iniciar Sesión</a></h4>  
+             
+      </div>
+    </div>
+  </header>
+
+  <div class="nav-scroller py-1 mb-2">
+    <nav class="nav d-flex justify-content-between">
+      <a class="p-2 text-muted" href="#">Conócenos</a>
+      <a class="p-2 text-muted" href="#">Seminario Menor</a>
+      <a class="p-2 text-muted" href="#">Seminario Mayor</a>
+      <a class="p-2 text-muted" href="#">Quiero ayudar</a>
+      <a class="p-2 text-muted" href="#">Calendario</a>
+      <a class="p-2 text-muted" href="#">Noticias</a>
+      <a class="p-2 text-muted" href="#">Enlaces</a>
+      <a class="p-2 text-muted" href="#">Opinion</a>
+     
+    </nav>
+  </div>
+ <div id="testimoniales" class="carousel slide" data-ride="carousel">
+                                <ol class="carousel-indicators">
+                                    <li data-target="#testimoniales" data-slide-to="0" class="active"></li>
+                                    <li data-target="#testimoniales" data-slide-to="1"></li>
+                                    <li data-target="#testimoniales" data-slide-to="2"></li>
+                                </ol>
+                                <div class="carousel-inner">
+                                    <div class="carousel-item active">
+                                      <img src="img/03.png" class="d-block" width="1120" height="475" />                       
+                                    </div>
+                                    <div class="carousel-item">                                            
+                                      <img src="img/02.png" class="d-block" width="1120" height="475" />
+                                                                              </div>
+                                    <div class="carousel-item">                                            
+                                      <img src="img/04.png" class="d-block" width="1120" height="475" />
+                                                    </div>
+                                            </div>
+                                    </div>
+                                </div> <!--.carousel-inner-->
+
+  <div class="jumbotron p-4 p-md-5 text-white rounded bg-dark">
+    <div class="col-md-6 px-0">
+      <h1 class="display-4 font-italic"> El llamado </h1>
+      <p class="lead my-3">  “No me han elegido ustedes a mi, soy yo quien los ha elegido” (Jn. 15,16). </p>
+      <p class="lead mb-0"><a  class="text-white font-weight-bold">Continuar leyendo</a></p>
+    </div>
+  </div>
+
+  <div class="row mb-2">
+    <div class="col-md-6">
+      <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        
+        <div class="col-auto d-none d-lg-block">
+          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><img src="img/03.png" class="d-block" width="20" height="20" </img></svg>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-6">
+      <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        
+        <div class="col-auto d-none d-lg-block">
+         
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+   
+    </div><!-- /.blog-main -->
+
+    <aside class="col-md-4 blog-sidebar">
+  
+      <div class="p-4">
+        <h4 class="font-italic">Redes Sociales en Facebook</h4>
+        <ol class="list-unstyled">
+          <li><a href="https://www.facebook.com/profile.php?id=100009443515033"> Pastoral Vocacional </a></li>
+          <li><a
+           href="https://www.facebook.com/Seminario-Menor-De-Ntra-Sra-Del-Carmen-Di%C3%B3cesis-de-Papantla-108500224271026">Seminario Menor del Carmen</a></li>
+           <li> 
+<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FSeminario-Menor-De-Ntra-Sra-Del-Carmen-Di%25C3%25B3cesis-de-Papantla-108500224271026&tabs=timeline&width=350&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=false&appId" width="350" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+           </li>
+          <li><a href="https://www.facebook.com/Seminario-Mayor-De-La-Inmaculada-Concepcion-134190930536377">Seminario Mayor de la Inmaculada</a></li>
+        </ol>
+      </div>
+    </aside><!-- /.blog-sidebar -->
+
+  </div><!-- /.row -->
+
+</main><!-- /.container -->
+
+
+    <div class="beneficios bg-dark mt-5">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-4 text-center text-light beneficio">
+                       <h3> Pastoral Vocacional de la Diosesis de Papantla  </h3>                     
+                       <p> </p>    
+                       <p>Pbro. Rafael Rodríguez Lucas. Encargado Diocesano de la Pastoral Vocacional. </i> 232 123 33 28</li></p>   
+                       <p>Pbro. Felipe Cortés Cruz. Auxiliar Diocesano de la Pastoral Vocacional.  232 123 3328 231 120 2996
+                     </div></p>   
+                <div class="col-md-4 text-center text-light beneficio">                       
+                        <h3> Seminaro Menor <span>de del Carmen </span></h3>
+                         
+                       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3738.2616046921466!2d-97.31761743554489!3d20.45444573631229!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85da442ef44d0763%3A0x5853ce6324ebf543!2sSeminario%20Menor%20del%20C%C3%A1rmen%2C%20Heroico%20Colegio%20Militar%2C%20Barrio%20del%20Naranjo%2C%2093400%20Papantla%20de%20Olarte%2C%20Ver.!5e0!3m2!1ses-419!2smx!4v1610991968923!5m2!1ses-419!2smx" width="300" height="200" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                       <p>  Barrio del Naranjo 93400 Papantla de Olarte, Ver.  </p>
+                </div>
+                <div class="col-md-4 text-center text-light beneficio">
+                        <h3> Seminario Mayor <span>de la Inmaculada</span></h3> 
+                        
+                      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3753.178448948308!2d-97.34701648508701!3d19.83240098665856!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85dae9dc9eb746bd%3A0x6951569b47b1c8af!2sSeminario%20Mayor%20de%20la%20Inmaculada%20Concepci%C3%B3n!5e0!3m2!1ses-419!2smx!4v1610991775389!5m2!1ses-419!2smx" width="300" height="200" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                      <p> Barrio de Chignaulingo, 73820 Teziutlán, Pue. </p>
+                      
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+<footer class="blog-footer">
+
+  <p> <a href="https://www.facebook.com/profile.php?id=100009443515033">Pastoral Vocacional de la Diócesisde Papantla</a> "Joven ¡No tengan miedo de ese amor que gasta la vida!” - Papa Francisco</a>.</p>
+  <p>
+    <a href="#">Programación Web PHJG CFM AHLM</a>
+  </p>
+</footer>
+
+
+<div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="Login" aria-hidden="true">
+            <div class="modal-dialog modal-sm">
+
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title" id="Login">Iniciar Sesión<h>
+                        <button type="button" class="btn btn-danger" data-dismiss="modal" aria-label="Cerrar">
+          <span aria-hidden="true">&times;</span>
+        </button>
+                    </div>
+                    <div class="modal-body">
+                        <form id="form_login"  method="post">
+                            <div class="form-group">
+                                <input type="text" name="user" class="form-control" id="user" placeholder="Correo" pattern="[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{1,5}">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" required="" name="pass" class="form-control" id="pass" placeholder="Contraseña" minlength="8" maxlength="10" pattern="^[A-Z][a-zA-Z0-9]+">
+                            </div>
+
+                            <p class="text-center">
+                                <button type="submit" class="btn btn-primary"><i class="fa fa-sign-in"></i> Entrar</button>
+                            </p>
+
+                        </form>
+                        <div id="msg_error" class="alert alert-danger" role="alert" style="display: none;" ></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+<div class="modal fade" id="registrar-modal" tabindex="-1" role="dialog" aria-labelledby="Login" aria-hidden="true">
+            <div class="modal-dialog modal-sm">
+
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title" id="Login">Registrarse<h>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar">
+          <span aria-hidden="true">&times;</span>
+        </button>
+                    </div>
+                    <div class="modal-body">
+                        <form id="form_registrar"  method="post">
+                          <div class="form-group">
+                                <input type="text" name="nombre" required="" class="form-control" minlength="3" id="nombre" placeholder="Nombre" pattern="[a-zA-Z]+">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" name="app" required="" class="form-control" minlength="3" id="app" placeholder="Apellido Paterno" pattern="[a-zA-Z]+">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" name="apm" required="" class="form-control" minlength="3" id="apm" placeholder="Apellido Materno" pattern="[a-zA-Z]+">
+                            </div>
+                            <div class="form-group">
+                              <label for="" class="control-label col-xs-5">Fecha Nacimiento:</label>
+                              <input class="form-control" required="" type="date" id="fnac" name="fnac" placeholder="" >  
+                            </div>
+                            <div class="form-group">
+                                <input type="text" name="correo"  class="form-control" id="correo" placeholder="Correo" pattern="[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{1,5}">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" required="" name="contra" class="form-control" id="contra" placeholder="Contraseña" minlength="8" maxlength="10" pattern="^[A-Z][a-zA-Z0-9]+">
+                            </div>
+
+                            <p class="text-center">
+                                <button type="submit" class="btn btn-primary"><i class="fa fa-sign-in"></i> Registrar</button>
+                            </p>
+
+                        </form>
+                        <div id="msgR_error" class="alert alert-danger" role="alert" style="display: none;" ></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/login.js"></script>
+
+
+
+  </body>
+</html>
